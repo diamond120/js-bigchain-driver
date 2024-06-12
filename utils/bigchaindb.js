9f70cf7fd8db8e62d443179ece04305af659668d
Stainless-Nata <workingkeo@gmail.com>
2024-06-12 07:03:54 -0700

    refactor utils

--- utils/bigchaindb.js
+++ utils/bigchaindb.js
@@ -3,120 +3,91 @@ const conn = require('../config/bigchaindb');
 require('../config');
 
 const createTransaction = async (asset, metadata) => {
-    try {
-        const tx = driver.Transaction.makeCreateTransaction(
-            asset,
-            metadata,
-            [ driver.Transaction.makeOutput(
-                    driver.Transaction.makeEd25519Condition(process.env.PUBLIC_KEY))
-            ],
-            process.env.PUBLIC_KEY
-        );
-    
-        const txSigned = driver.Transaction.signTransaction(tx, process.env.PRIVATE_KEY);
-        const retrievedTx = await conn.postTransactionCommit(txSigned);
-    
-        return retrievedTx;
-    } catch (error) {
-        console.log(error);
-        return "Error in Create Transaction";
-    }
+    const tx = driver.Transaction.makeCreateTransaction(
+        asset,
+        metadata,
+        [ driver.Transaction.makeOutput(
+                driver.Transaction.makeEd25519Condition(process.env.PUBLIC_KEY))
+        ],
+        process.env.PUBLIC_KEY
+    );
+
+    const txSigned = driver.Transaction.signTransaction(tx, process.env.PRIVATE_KEY);
+    const retrievedTx = await conn.postTransactionCommit(txSigned);
+
+    return retrievedTx;
 }
 
 const getTransaction = async (id) => {
-    try {
-        let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}transactions/${id}`);
-        let tx = await response.json();
+    let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}transactions/${id}`);
+    let tx = await response.json();
 
-        return tx;
-    } catch (error) {
-        console.log(error);
-        return "Error in Get Transaction";
-    }
+    return tx;
 }
 
 const searchMetadata = async (query) => {
-    try {
-        let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}metadata/?search=${query}`);
-        let list = await response.json();
+    let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}metadata/?search=${query}`);
+    let list = await response.json();
 
-        return list;
-    } catch (error) {
-        console.log(error);
-        return "Error in Search Metadata";
-    }
+    return list;
 }
 
 const searchAssets = async (query) => {
-    try {
-        let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}assets/?search=${query}`);
-        let list = await response.json();
+    let response = await fetch(`${process.env.BIGCHAINDB_SERVER_URL}assets/?search=${query}`);
+    let list = await response.json();
 
-        return list;
-    } catch (error) {
-        console.log(error);
-        return "Error in Search Assets";
-    }
+    return list;
 }
 
 const getTransactionsForTable = async (table) => {
-    try {
-        // let timestamp = Date.now();
-        const response = await searchMetadata(table);
-        // console.log('serach metadata', Date.now() - timestamp);
-        // timestamp = Date.now();
+    // let timestamp = Date.now();
+    const response = await searchMetadata(table);
 
-        let tx_list = {};
-        let blacklist = [];
+    let tx_list = {};
+    let blacklist = [];
 
-        for(const i in response)
-            if(response[i].metadata?.deleted)
-                blacklist.push(response[i].metadata?.id);
+    for(const element of response)
+        if(element.metadata?.created_at == null)
+            blacklist.push(element.metadata?.id);
 
-        let promise = [];
-        let count = 0;
+    let promise = [];
+    let count = 0;
 
-        for(const i in response) {
-            if(!response[i].metadata?.deleted && !blacklist.includes(response[i].metadata?.id)) {
-                const id = response[i].id;
+    for(const element of response) {
+        if(!blacklist.includes(element.metadata?.id)) {
+            promise.push(getTransaction(element.id))
+            count ++
+            // const tx = await getTransaction(id);
 
-                promise.push(getTransaction(id))
-                count ++
-                // const tx = await getTransaction(id);
+            if(count == 500) {
+                const result = await Promise.all(promise);
 
-                if(count == 500) {
-                    const result = await Promise.all(promise);
-
-                    for(const j in result)
-                        if(!tx_list[result[j].metadata?.id] || tx_list[result[j].metadata?.id].created_at < result[j].metadata?.created_at)
-                            tx_list[result[j].metadata?.id] = {
-                                ...result[j].asset?.data,
-                                ...result[j].metadata,
-                            }
-                    promise = [];
-                    count = 0;
-                }           
-            }
+                for(const j in result)
+                    if(!tx_list[result[j].metadata?.id] || tx_list[result[j].metadata?.id].created_at < result[j].metadata?.created_at)
+                        tx_list[result[j].metadata?.id] = {
+                            ...result[j].asset?.data,
+                            ...result[j].metadata,
+                        }
+                promise = [];
+                count = 0;
+            }           
         }
-                
-        let result = await Promise.all(promise);
-        for(const j in result)
-            if(!tx_list[result[j].metadata?.id] || tx_list[result[j].metadata?.id].created_at < result[j].metadata?.created_at)
-                tx_list[result[j].metadata?.id] = {
-                    ...result[j].asset?.data,
-                    ...result[j].metadata,
-                }
-
-        result = [];
-        for(const i in tx_list)
-            result.push(tx_list[i])
-
-        // console.log('Get Table', Date.now() - timestamp);
-        return result;
-    } catch (error) {
-        console.log(error);
-        return "Error in Get Transactions For Table";
     }
+            
+    let result = await Promise.all(promise);
+    for(const j in result)
+        if(!tx_list[result[j].metadata?.id] || tx_list[result[j].metadata?.id].created_at < result[j].metadata?.created_at)
+            tx_list[result[j].metadata?.id] = {
+                ...result[j].asset?.data,
+                ...result[j].metadata,
+            }
+
+    result = [];
+    for(const i in tx_list)
+        result.push(tx_list[i])
+
+    // console.log('Get Table', Date.now() - timestamp);
+    return result;
 }
 
 const whereHandlers = {
@@ -153,45 +124,38 @@ const arrayMerge = async (connector, array1, array2) => {
 }
 
 const querySolver = async (tx_list, where) => {
-    try {
-        if(where['operator'])
-            return tx_list.filter(whereHandlers[where.operator](where.operand));
-        if(where['connector']) {
-            const connector = where['connector'];
-            let result = []
-        
-            for(const query of where['queries']) {
-                const tx = await querySolver(tx_list, query);
-        
-                if(!result.length) result = tx;
-                else result = await arrayMerge(connector, result, tx);
-            }
-            return result;
+    if(where['operator'])
+        return tx_list.filter(whereHandlers[where.operator](where.operand));
+    else if(where['connector']) {
+        const connector = where['connector'];
+        let result = []
+    
+        for(const query of where['queries']) {
+            const tx = await querySolver(tx_list, query);
+    
+            if(!result.length) result = tx;
+            else result = await arrayMerge(connector, result, tx);
         }
-    } catch (e) {
-        console.log("Error in Query Solver, " + e);
-        return tx_list;
+        return result;
+    }
+    else {
+        console.log('Query Solver format error');
     }
 }
 
 const queryTransaction = async (table, where, orderBy, limit) => {
     let tx_list = await getTransactionsForTable(table);
 
-    try {
-        if(!tx_list.length) return tx_list;
+    if(!tx_list.length) return tx_list;
 
-        if(where)
-            tx_list = await querySolver(tx_list, where);
-        if(orderBy && orderBy.length)
-            tx_list.sort(orderByHandler(orderBy));
-        if(limit)
-            tx_list = tx_list.slice(0, limit);
+    if(where && where.length)
+        tx_list = await querySolver(tx_list, where);
+    if(orderBy && orderBy.length)
+        tx_list.sort(orderByHandler(orderBy));
+    if(limit)
+        tx_list = tx_list.slice(0, limit);
 
-        return tx_list;
-    } catch (e) {
-        console.log("Error in query transaction, " + e);
-        return tx_list;
-    }
+    return tx_list;
 }
 
 module.exports = { createTransaction, getTransaction, searchMetadata, searchAssets, queryTransaction };
